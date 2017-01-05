a4814eb42e7bc00e1cfdb91d7930a8f3e12bd493
michael grandison <michael.grandison@gmail.com>
2017-01-05 15:23:32 +0000

    angular test

---
+++ src/js/controllers/main.js
@@ -0,0 +1,8 @@
+angular.module('portfolio')
+  .controller('MainController', MainController);
+
+
+MainController.$inject = ['$auth', '$state', '$rootScope'];
+function MainController($auth, $state, $rootScope) {
+  const main = this;
+}
