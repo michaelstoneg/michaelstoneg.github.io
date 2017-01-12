7e8790f605eb2fc5137226a04c7b02dff45fb532
michael grandison <michael.grandison@gmail.com>
2017-01-12 23:40:57 +0000

    jp for work

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -5,11 +5,22 @@ angular.module('portfolio')
 MainController.$inject = ['$state', '$rootScope'];
 function MainController($state, $rootScope) {
   const main = this;
+  main.popupShow = false;
+  console.log(main.popupShow);
 
   function secureState(e, toState, toParams) {
     main.stateName = toState.name;
     console.log(main.stateName);
+    main.stateChange = true;
+    console.log('changing state. light up stats');
   }
 
   $rootScope.$on('$stateChangeStart', secureState);
+
+  function popupShow () {
+    main.popupShow = true;
+    console.log(main.popupShow);
+  }
+
+  main.popupShowFunc = popupShow;
 }
